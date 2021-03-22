fb8da73f5f68386cd9985ffce330e7d4bb41354d
Daniel Frieser <35167698+macTigers@users.noreply.github.com>
2021-03-22 09:48:53 +0100

    Add files via upload

---
+++ mod4_solution/SpeakHello.js
@@ -0,0 +1,9 @@
+/* Script for Coursera Test Module 4 @ macTigers*/
+(function (window) {
+  var helloSpeaker = {};
+  var speakWord = "Hello";
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+  window.helloSpeaker = helloSpeaker;
+})(window);
